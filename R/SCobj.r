28f87261ac5c767d9c3c463d153c27fc2e2d0e8b
John <john.bassett@ki.se>
2022-06-14 20:39:03 +0200

    SC_obj class to NAMESPACE

--- R/SCobj.r
+++ R/SCobj.r
@@ -1,3 +1,16 @@
+#' SC_obj
+#'
+#' @slot raw.data ANY.
+#' @slot assay list.
+#' @slot meta.data data.frame.
+#' @slot graphs list.
+#' @slot ident factor.
+#' @slot est_doublet_rate numeric.
+#'
+#' @return
+#' @export
+#'
+#' @examples
 SC_obj <- setClass(Class = "SC_obj",
                   slots = c(
                     raw.data = 'ANY',
@@ -69,27 +82,71 @@ setMethod(f = "dimnames",
           })
 
 ###############################################################################
+#' select
+#'
+#' @param x
+#' @param meta.logical
+#'
+#' @return
+#' @export
+#'
+#' @examples
 select <- function(x, meta.logical){ #this is wonky af
   y <- x@meta.data[x@meta.data[,meta.logical],]
   y <- rownames(y) #TODO make this less error prone
   return(x@raw.data[,y])
 }
 
+#' addGraph
+#'
+#' @param x
+#' @param value
+#'
+#' @return
+#' @export
+#'
+#' @examples
 addGraph <-function(x, value){
   x@graphs <- c(x@graphs, list(value))
   return(x)
 }
 
+#' addMetaData
+#'
+#' @param x
+#' @param value
+#' @param col.name
+#'
+#' @return
+#' @export
+#'
+#' @examples
 addMetaData <- function(x, value, col.name){
   x@meta.data[col.name] <- value
   return(x)
 }
 
+#' remove1
+#'
+#' @param data.10x
+#'
+#' @return
+#' @export
+#'
+#' @examples
 remove1 <- function(data.10x){
   dimnames(data.10x)[[2]] <- sub("-1", "", dimnames(data.10x)[[2]])
   return(data.10x)
 }
 
+#' initializeMetaData
+#'
+#' @param data.10x
+#'
+#' @return
+#'
+#'
+#' @examples
 initializeMetaData <- function(data.10x){ ##function for initializing metadata from count matrix for sc_obj
   meta.data<- data.frame(Matrix::colSums(data.10x))
   meta.data[,2] <- diff(data.10x@p)
@@ -97,6 +154,15 @@ initializeMetaData <- function(data.10x){ ##function for initializing metadata f
   return(meta.data)
 }
 
+#' newSC_obj
+#'
+#' @param data.10x
+#' @param remove1
+#'
+#' @return
+#' @export
+#'
+#' @examples
 newSC_obj <- function(data.10x, remove1 = T){
   if(remove1 == T){
     data.10x <- remove1(data.10x)
@@ -106,11 +172,28 @@ newSC_obj <- function(data.10x, remove1 = T){
       meta.data = initializeMetaData(data.10x))
 }
 
+#' set_doublet_rate
+#'
+#' @param sc_obj
+#' @param doublet_rate
+#'
+#' @return
+#' @export
+#'
+#' @examples
 set_doublet_rate <- function(sc_obj, doublet_rate){
   sc_obj@est_doublet_rate = doublet_rate
   return(sc_obj)
 }
 
+#' doublet_rate
+#'
+#' @param sc_obj
+#'
+#' @return
+#' @export
+#'
+#' @examples
 doublet_rate <- function(sc_obj){
   return(sc_obj@est_doublet_rate)
 }
